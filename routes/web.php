7c6c67c624fca6da41bf26fd959fa592a3c88e27
yamdev07 <yamdyamd080@gmail.com>
2026-04-28 09:07:20 +0100

    add landing page and

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin'       => Route::has('login'),
-        'canRegister'    => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion'     => PHP_VERSION,
+        'canLogin'    => Route::has('login'),
+        'canRegister' => Route::has('register'),
     ]);
 });
 
